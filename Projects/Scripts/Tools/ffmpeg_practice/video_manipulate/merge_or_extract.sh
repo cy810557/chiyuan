72ecd383105de171ed93353d782efd05ddc0338b
cy810557 <634755812@qq.com>
2019-10-19 16:36:47 +0800

    Create mp4 using frames without formated names

--- Projects/Scripts/Tools/ffmpeg_practice/video_manipulate/merge_or_extract.sh
+++ Projects/Scripts/Tools/ffmpeg_practice/video_manipulate/merge_or_extract.sh
@@ -10,3 +10,6 @@ ffmpeg -i <infile> -acodec copy -vn <outfile.m4a>
 # merge
 ffmpeg -i <in.mp4> -i <in.m4a> -c copy out.mp4
 # how to add audio on top of a video file with sound?
+
+# create video using frames
+ffmpeg -pattern_type glob -r 3 -i '*.jpg' -c:v libx264 -profile:v high -pix_fmt yuv420p output.mp4
